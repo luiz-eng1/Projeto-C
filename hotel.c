86c9109e714334938e7b65c59d33c07cd56351f0
Luiz Eduardo <lduducamposs@gmail.com>
2025-11-15 12:01:46 -0300

    função Cadastrar Cliente e contadores

--- hotel.c
+++ hotel.c
@@ -1,27 +1,33 @@
 //Sistema Hotel Descanso Garantido  
 #include <stdio.h>
 
-
+// structs
 struct Cliente{
     int idCliente;
     char nome[50];
     char endereço[100];
-    int telefone;
+    char telefone[20];
 
 };
 
 struct Funcionario{
     int idFuncionario;
     char nome[50];
-    int telefone;
+    char telefone[20];
     char cargo[50];
     float salario;
 };
 
+struct Data{
+    int dia;
+    int mes;
+    int ano;
+};
+
 struct Estadia{
     int idEstadia;
-    int dataEntrada;
-    int dataSaida;
+    struct Data dataEntrada;
+    struct Data dataSaida;
     int qntDiarias;
     int idCliente;
     int nQuarto;
@@ -33,4 +39,39 @@ struct Quarto{
     int qtdHospedes;
     float valorDiaria;
     char status[20];
+};
+
+
+// criando o vetor de structs
+struct Cliente clientes[100];
+struct Funcionario funcionarios[100];
+struct Data datas[100];
+struct Estadia estadias[100];
+struct Quarto quartos[100];
+
+
+
+//contadores    (para usar Ex. clientes[i].nome, acessando do indice teria que usar laço, ou criar um contador  <-)
+int ultimoCodigoCliente = 0;
+int qtdClientes = 0;
+int qtdFuncionarios = 0;
+int qtdQuartos = 0;
+
+
+
+// funções 
+
+void CadastrarCliente(){
+
+    printf("Nome do Cliente: ");
+    fgets(clientes[qtdClientes].nome, 50, stdin);
+
+    printf("Endereço do cliente: ");
+    fgets(clientes[qtdClientes].endereço, 50, stdin);
+
+    printf("Telefone do cliente: ");
+    fgets(clientes[qtdClientes].telefone, 20, stdin);
+
+    ultimoCodigoCliente++;
+    clientes[qtdClientes].idCliente == ultimoCodigoCliente;
 };
