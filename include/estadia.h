8f5c126786d9787dc63af46c9ea1b3d7d7cfd9d6
Eli Júnior Dias <elijuniordias098@gmail.com>
2025-11-19 18:49:16 -0300

    Adiciona definição da estrutura Estadia e funções para gerenciamento de estadias

---
+++ include/estadia.h
@@ -0,0 +1,24 @@
+#ifndef ESTADIA_H
+#define ESTADIA_H
+
+#include "../include/cliente.h"
+#include "../include/quarto.h"
+
+#define ARQ_ESTADIAS "estadias.bin"
+
+typedef struct{
+    int idEstadia;
+    int idCliente;
+    int numeroQuarto;
+    char dataEntrada[11];
+    char dataSaida[11];
+    int qtdDiarias;
+}Estadia;
+
+int gerarCodigoEstadia();
+int calcularDiarias(char entrada[], char saida[]);
+int verificarConflito(int numeroQuarto, char entrada[], char saida[]);
+void cadastrarEstadia();
+void finalizarEstadia();
+
+#endif 
